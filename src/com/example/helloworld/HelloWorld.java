794b5ea90ab1d410e453842b168e62e638d1344e
David <dyang10@madisoncollege.edu>
2016-02-02 10:49:58 -0600

    Initial Commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by David on 1/26/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("it' me, Wombat!");
+    }
+}
